446cf998d50e3748e94571e79db0d211957a7b76
tamnna jakhar <tamnnajakhar05@gmail.co>
2026-05-04 12:36:04 +0530

    some changes on position

--- Frontend/src/modules/shared/home/components/Hero.jsx
+++ Frontend/src/modules/shared/home/components/Hero.jsx
@@ -34,7 +34,7 @@ export default function Hero() {
       <div className={styles.heroBg} />
 
       <div className="absolute left-1/2 -translate-x-1/2 w-[80.83%] h-[60%] top-[35%] px-4">
-        <div className="bg-white rounded-[10px] shadow-2xl w-full !pb-14 h-89 md:p-8">
+        <div className="bg-white rounded-[10px] shadow-2xl w-full !pb-14 h-86 md:p-8">
           <Tabs
             tabs={enabledTabs}
             activeTab={activeTab}
--- Frontend/src/modules/shared/home/components/SearchButton.jsx
+++ Frontend/src/modules/shared/home/components/SearchButton.jsx
@@ -2,7 +2,7 @@
 
 export default function SearchButton({ onSearch }) {
   return (
-    <div className="absolute left-1/2 -translate-x-1/2 top-[67%] translate-y-1/2">
+    <div className="absolute left-1/2 -translate-x-1/2 top-[64%] translate-y-1/2">
       <button
         onClick={onSearch}
         className="text-white px-12 py-4 rounded-xl text-base font-semibold shadow-lg hover:opacity-90 hover:scale-[1.02] active:scale-[0.98] w-[400px] h-16 flex items-center justify-center text-2xl transition-all duration-200"
