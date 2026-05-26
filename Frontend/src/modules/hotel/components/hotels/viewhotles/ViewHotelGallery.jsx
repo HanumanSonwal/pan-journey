1c1ae32a67b21dfa9b02cc075f5827bcb0653098
tamnna jakhar <tamnnajakhar05@gmail.co>
2026-05-26 15:50:27 +0530

    save changes

--- Frontend/src/modules/hotel/components/hotels/viewhotles/ViewHotelGallery.jsx
+++ Frontend/src/modules/hotel/components/hotels/viewhotles/ViewHotelGallery.jsx
@@ -21,7 +21,13 @@ const ViewHotelGallery = ({ images = [], onOpen }) => {
     <div className="grid h-full grid-cols-1 gap-3 md:grid-cols-2">
       <div
         onClick={onOpen}
-        className="relative cursor-pointer overflow-hidden rounded"
+        className="relative cursor-pointer overflow-hidden rounded
+        h-[260px]
+sm:h-[320px]
+md:h-[420px]
+lg:h-[520px]
+xl:h-full
+2xl:h-full"
       >
         <Image
           src={mainImage}
