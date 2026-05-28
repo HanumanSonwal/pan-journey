549d4e17096b79f928b526cec70430fa58cfe82b
Aditya Sonwal <asonwal5@gmail.com>
2026-05-28 12:04:16 +0530

    dashbaord serch api correction

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,6 @@ import destinationRoutes from "./modules/exploreDesitanation/desitanation.routes
 import hotelSearch from "./modules/hotel/hotel.route.js";
 import hotelDetails from "./modules/hotel/hotelDetails/hotel.routes.js";
 import tempbookingRoutes from "./modules/hotel/hotelTempBooking/booking.routes.js";
-import dashboardhotelsearch from "./modules/dashboardHotellSearch/hotel.routes.js";
 import seoContentRoutes from "./modules/hotel/seo/hotelcityseo/seoContent.routes.js";
 import mediaRoutes from "./modules/media/media.routes.js";
 import countryRoutes from "./modules/priceMarkup/countryData/country.routes.js";
@@ -58,7 +57,7 @@ app.use("/api/v1/cms", cmsRoutes);
 app.use("/api/v1/roles", roleRoutes);
 app.use("/api/v1/Seacrhcity", citySearch);
 app.use("/api/v1/Hotels", hotelSearch);
-app.use( "/api/v1",tempbookingRoutes);
+app.use("/api/v1", tempbookingRoutes);
 
 app.use("/api/v1/customer/auth/", otpRoutes);
 app.use("/api/v1/customer/profile", profileRoutes);
