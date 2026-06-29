1dd8a4fe26b12c7b69a09c99b0148389922ed9c7
Aditya Sonwal <asonwal5@gmail.com>
2026-06-29 12:05:32 +0530

    changes done

--- admin-dashboard/middleware.js
+++ admin-dashboard/middleware.js
@@ -1,22 +1,26 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
+
+// export function middleware(req) {
+//   const token = req.cookies.get("accessToken")?.value;
+//   const { pathname } = req.nextUrl;
+
+//   const isPublic = pathname === "/";
+
+//   if (!token && !isPublic) {
+//     return NextResponse.redirect(new URL("/", req.url));
+//   }
+
+//   if (token && isPublic) {
+//     return NextResponse.redirect(new URL("/dashboard", req.url));
+//   }
+
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ["/", "/dashboard/:path*"],
+// };
 
 export function middleware(req) {
-  const token = req.cookies.get("accessToken")?.value;
-  const { pathname } = req.nextUrl;
-
-  const isPublic = pathname === "/";
-
-  if (!token && !isPublic) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
-
-  if (token && isPublic) {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
-  }
-
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ["/", "/dashboard/:path*"],
-};
