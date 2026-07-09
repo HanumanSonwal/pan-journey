f9906e5718b0e97a1470927a131763df53cbb111
Aditya Sonwal <asonwal5@gmail.com>
2026-07-09 16:15:11 +0530

    hydration error resolve

--- Frontend/src/modules/hotel/hooks/useBookingDetails.js
+++ Frontend/src/modules/hotel/hooks/useBookingDetails.js
@@ -10,15 +10,10 @@ import {
 export const useBookingDetails = (bookingRefNo) => {
   return useQuery({
     queryKey: ["hotel-booking-details", bookingRefNo],
-
     queryFn: () => getBookingDetails(bookingRefNo),
-
     enabled: !!bookingRefNo,
-
     staleTime: 1000 * 60 * 5,
-
     retry: 1,
-
     refetchOnWindowFocus: false,
   });
 };
