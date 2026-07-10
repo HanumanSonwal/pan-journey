2f9ebf56a61b1f0bc1db36cfd2690b5ab28a680f
tamnna jakhar <tamnnajakhar05@gmail.co>
2026-07-10 12:39:32 +0530

    responsive done

--- Frontend/src/components/common/Header.jsx
+++ Frontend/src/components/common/Header.jsx
@@ -273,13 +273,13 @@ export default function Header() {
           {!session ? (
             <button
               onClick={() => setOpen(true)}
-              className={`bg-offer-gradient flex items-center rounded-lg text-white transition-all duration-300 hover:-translate-y-0.5 hover:shadow-lg ${isMobile
+              className={`bg-offer-gradient flex items-center rounded-lg text-white! transition-all duration-300 hover:-translate-y-0.5 hover:shadow-lg ${isMobile
                 ? "gap-3 px-4 py-2 text-[13px]"
                 : "gap-2 px-3 py-2 text-sm"
                 }`}
             >
               <UserOutlined
-                className={isMobile ? "text-[12px]" : "text-[18px]"}
+                className={isMobile ? "text-[12px]" : "text-[18px] "}
               />
 
               {!isMobile && <span>Login</span>}
