a81dc241a3e305660bb7c6b4b77a958970943ddb
Aditya Sonwal <asonwal5@gmail.com>
2026-07-24 17:04:40 +0530

    error resolve

--- Frontend/src/app/page.js
+++ Frontend/src/app/page.js
@@ -138,8 +138,6 @@ export default async function Page() {
         />
       )}
 
-
-
       <ScrollToTopButton />
       <div className="relative">
         <Hero />
--- Frontend/src/modules/shared/home/components/hero_section/TrustSection.jsx
+++ Frontend/src/modules/shared/home/components/hero_section/TrustSection.jsx
@@ -66,33 +66,7 @@ const trustItems = [
 
 export default function TrustSection() {
   return (
-    <section
-      className="
-    absolute
-    left-1/2
-    -translate-x-1/2
-
-    bottom-[-195px]
-
-    z-20
-
-    w-[92%]
-    sm:w-[94%]
-
-    min-[1000px]:w-[90%]
-    min-[1200px]:w-[86%]
-
-    xl:w-[82%]
-    2xl:w-[82%]
-
-    bg-[#fafafa]
-    border
-    border-[#e5e7eb]
-
-    rounded-lg
-    overflow-hidden
-  "
-    >
+    <section className="absolute bottom-[-195px] left-1/2 z-20 w-[92%] -translate-x-1/2 overflow-hidden rounded-lg border border-[#e5e7eb] bg-[#fafafa] min-[1000px]:w-[90%] min-[1200px]:w-[86%] sm:w-[94%] xl:w-[82%] 2xl:w-[82%]">
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
         {trustItems.map((item, index) => (
           <div
