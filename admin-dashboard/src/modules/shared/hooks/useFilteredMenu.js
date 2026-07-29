001f5d248f762b24d49a9f2fcd3cc347484f0594
Aditya Sonwal <asonwal5@gmail.com>
2026-07-29 15:27:43 +0530

    chnages done in menu config

--- admin-dashboard/src/modules/shared/hooks/useFilteredMenu.js
+++ admin-dashboard/src/modules/shared/hooks/useFilteredMenu.js
@@ -1,7 +1,36 @@
 "use client";
 
+import { bottomMenuItems } from "@/config/bottomMenuConfig";
 import { menuItems } from "@/config/menuConfig";
 import { useAuthStore } from "@/modules/auth/store/auth.store";
+
+const filterMenus = (items, canAccessModule) => {
+  return items
+    .map((item) => {
+      // Logout ko filter mat karo
+      if (item.key === "logout") {
+        return item;
+      }
+
+      if (item.children) {
+        const children = item.children.filter((child) =>
+          canAccessModule(child.module),
+        );
+
+        if (children.length > 0) {
+          return {
+            ...item,
+            children,
+          };
+        }
+
+        return null;
+      }
+
+      return canAccessModule(item.module) ? item : null;
+    })
+    .filter(Boolean);
+};
 
 export const useFilteredMenu = () => {
   const { permissions = {}, user } = useAuthStore();
@@ -15,21 +44,8 @@ export const useFilteredMenu = () => {
     return !!permissions?.[module]?.read;
   };
 
-  return menuItems
-    .map((item) => {
-      if (item.children) {
-        const children = item.children.filter((child) =>
-          canAccessModule(child.module),
-        );
-
-        if (children.length > 0) {
-          return { ...item, children };
-        }
-
-        return null;
-      }
-
-      return canAccessModule(item.module) ? item : null;
-    })
-    .filter(Boolean);
+  return {
+    menuItems: filterMenus(menuItems, canAccessModule),
+    bottomMenuItems: filterMenus(bottomMenuItems, canAccessModule),
+  };
 };
