e1c376d0c68b6b73b8a098687c2987641bd17ade
tamnna jakhar <tamnnajakhar05@gmail.co>
2026-08-04 15:02:20 +0530

    some changes

--- Frontend/src/modules/hotel/ScrollToTopButton.jsx
+++ Frontend/src/modules/hotel/ScrollToTopButton.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ArrowUpOutlined } from "@ant-design/icons";
+import { UpOutlined } from "@ant-design/icons";
 import { useEffect, useState } from "react";
 
 const ScrollToTopButton = () => {
@@ -28,33 +28,31 @@ const ScrollToTopButton = () => {
         onClick={scrollToTop}
         aria-label="Scroll To Top"
         className={`fixed right-6 bottom-20 z-[99] h-[42px] w-[42px] rounded-full transition-all duration-500 md:bottom-12 ${show
-            ? "translate-y-0 scale-100 opacity-100"
-            : "pointer-events-none translate-y-20 scale-75 opacity-0"
+          ? "translate-y-0 scale-100 opacity-100"
+          : "pointer-events-none translate-y-20 scale-75 opacity-0"
           }`}
       >
         {/* Animated Ring */}
         <span
-          className="absolute inset-0 animate-spin rounded-full buttion-background-color"
+          className="absolute inset-0 animate-spin rounded-full most-boder-colour "
           style={{
             animationDuration: "4s",
           }}
         />
 
         {/* Main Button */}
-        <span className="absolute inset-[4px] flex items-center justify-center overflow-hidden rounded-full buttion-background-color">
+        <span className="absolute inset-[4px] flex items-center justify-center overflow-hidden rounded-full ">
           {/* Gloss */}
           <span className="absolute left-1/2 top-1 h-6 w-10 -translate-x-1/2 rounded-full " />
 
           {/* Arrow */}
-          <ArrowUpOutlined className="relative z-10 text-[24px] !text-white drop-shadow-lg" />
+          <UpOutlined className="relative z-10 text-[24px] most-text-color drop-shadow-lg" />
         </span>
 
         {/* Outer Glow */}
         <span
           className="absolute inset-0 animate-pulse rounded-full"
-          style={{
-            boxShadow: "0 0 18px rgba(148, 170, 249, 0.45)",
-          }}
+
         />
       </button>
     </>
