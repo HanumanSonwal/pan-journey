3a87736605d77d6b5ba3cb20423dc9e1642f9651
Aditya Sonwal <asonwal5@gmail.com>
2026-08-05 11:23:25 +0530

    changes done

--- Backend/src/modules/exploreDesitanation/desitanation.routes.js
+++ Backend/src/modules/exploreDesitanation/desitanation.routes.js
@@ -1,27 +1,49 @@
 import express from "express";
-import { getHomeDestinationsController ,createDestination,
+import {
+  createDestination,
+  deleteDestination,
   getDestinations,
+  getHomeDestinationsController,
   updateDestination,
-  deleteDestination,} from "./desitanation.controller.js";
+} from "./desitanation.controller.js";
 
 const router = express.Router();
 
 router.get("/", getHomeDestinationsController);
 
-
-import {protect} from "../../middleware/auth.middleware.js";
+import { protect } from "../../middleware/auth.middleware.js";
 import { checkPermission } from "../../middleware/checkPermission.js";
 
 //const router = express.Router();
 
 // Admin Panel
-router.post("/createDestination", protect,checkPermission("destination", "write"), createDestination);
+router.post(
+  "/createDestination",
+  protect,
+  checkPermission("destination", "write"),
+  createDestination,
+);
 
-router.put("/:id", protect,checkPermission("destination", "update"), updateDestination);
+router.put(
+  "/:id",
+  protect,
+  checkPermission("destination", "update"),
+  updateDestination,
+);
 
-router.delete("/:id", protect,checkPermission("destination", "delete"), deleteDestination);
+router.delete(
+  "/:id",
+  protect,
+  checkPermission("destination", "delete"),
+  deleteDestination,
+);
 
 // Public
-router.get("/admin",protect,checkPermission("destination", "read"), getDestinations);
+router.get(
+  "/admin",
+  protect,
+  checkPermission("destination", "read"),
+  getDestinations,
+);
 
 export default router;
