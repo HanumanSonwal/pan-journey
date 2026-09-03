37224ac714927aae3b476032ac4af0cef04ddde8
Himanshu Sharma <himanshu11752@gmail.com>
2026-09-03 14:49:24 +0530

    pagination

--- Backend/src/modules/hotel/hotelSearch/hotelSearch.repository.js
+++ Backend/src/modules/hotel/hotelSearch/hotelSearch.repository.js
@@ -1,34 +1,4 @@
 
-// import HotelSearch from "./hotelSearch.model.js";
-
-// export const saveHotelSearch = async (data) => {
-//   return HotelSearch.create(data);
-// };
-
-// export const getHotelSearchById = async (searchId) => {
-//   return HotelSearch.findOne({
-//     searchId,
-//   }).lean();
-// };
-
-// export const deleteExpiredHotelSearch = async () => {
-//   return HotelSearch.deleteMany({
-//     expiresAt: {
-//       $lt: new Date(),
-//     },
-//   });
-// };
-// export const findHotelSearchCache = async ({
-//   cacheKey,
-// }) => {
-//   return HotelSearch.findOne({
-//     cacheKey,
-//     expiresAt: {
-//       $gt: new Date(),
-//     },
-//   }).lean();
-// };
-
 
 import HotelSearch from "./hotelSearch.model.js";
 
